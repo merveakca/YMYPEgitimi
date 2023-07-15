c9757ff2e023a1437e6b7714e7c01d1c865da949
Taner Saydam <taner_and_you@hotmail.com>
2023-07-15 11:06:35 +0300

    .

--- 01.Degiskenler/Program.cs
+++ 01.Degiskenler/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello World!");
+
+
+//string,int,decimal,double,float,bool,DateTime,Object
+
+string name; //null
+int age; //0
+bool isActive; //false
+
+float n1 = 100;
+double n2 = 200;
+var n3 = n1 + n2;
+Console.WriteLine(n3.GetType());
+
+string nameLastName = "Taner Saydam";
+
+name = "asdasd";
+
+char karakter = 'A';
+string aciklama = "asdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
+
+Console.WriteLine(nameLastName);
+
+
+
+
+Console.ReadLine();
+
+
